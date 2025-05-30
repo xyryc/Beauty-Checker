f50c32258c2d6de7a78cdffd137962b7f4216914
Md Anik <amdanikzz@gmail.com>
2025-05-30 06:31:26 +0600

    added basic bottom tabbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,28 @@
 import SafeScreen from "@/components/SafeScreen";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "./globals.css";
 
 export default function RootLayout() {
+  const router = useRouter();
+  const isSignedIn = true;
+  const inAuthScreen = true;
+
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      if (!isSignedIn && !inAuthScreen) router.replace("/(auth)");
+      else if (isSignedIn && inAuthScreen) router.replace("/(tabs)");
+    });
+  }, []);
+
   return (
     <SafeAreaProvider>
       <SafeScreen>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(tabs)" />
         </Stack>
       </SafeScreen>
 
