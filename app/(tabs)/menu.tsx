c957d6f1179a29d8b1680c4f794fa950d4df1b3a
Md Anik <amdanikzz@gmail.com>
2025-06-11 05:15:25 +0600

    developed profile menu UI

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,5 +1,6 @@
 import SafeScreen from "@/components/SafeScreen";
 import {
+  AntDesign,
   Feather,
   FontAwesome6,
   MaterialCommunityIcons,
@@ -16,109 +17,126 @@ const Menu = () => {
   return (
     <SafeScreen>
       <View className="px-5 mt-6">
-        {/* account */}
-        <View>
+        {/* profile image and name */}
+        <TouchableOpacity
+          onPress={() => router.push("/menu/ProfileScreen")}
+          className="mx-auto"
+        >
+          <Image
+            style={{
+              width: 150,
+              height: 150,
+              borderRadius: 20,
+              borderColor: "#767676",
+              borderWidth: 0.5,
+            }}
+            contentFit="cover"
+            source="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
+          />
+
           <Text
-            className="text-2xl font-medium text-primary mb-4"
+            className="text-primary text-2xl font-medium mt-4"
             style={{ fontFamily: "Poppins" }}
           >
-            Account
+            Julian Assange
           </Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity
-            onPress={() => router.push("/menu/ProfileScreen")}
-            className="flex-row items-center justify-between py-2 mb-4"
-          >
-            <View className="flex-row items-center gap-2">
-              <Image
-                className="mr-2"
-                style={{ width: 40, height: 40, borderRadius: 50 }}
-                contentFit="cover"
-                source="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
-              />
+        {/* Account and support  */}
+        <View className="mt-5">
+          {/* account */}
+          <View>
+            <Text
+              className="text-2xl font-medium text-primary mb-4"
+              style={{ fontFamily: "Poppins" }}
+            >
+              Account
+            </Text>
 
-              <Text
-                className="text-primary text-xl font-medium"
-                style={{ fontFamily: "Poppins" }}
-              >
-                Julian Assange
-              </Text>
-            </View>
+            <TouchableOpacity className="flex-row items-center justify-between py-2">
+              <View className="flex-row items-center gap-4">
+                <AntDesign name="user" size={24} color="#767676" />
 
-            <View className="p-2">
-              <FontAwesome6 name="angle-right" size={20} color="#767676" />
-            </View>
-          </TouchableOpacity>
+                <Text className="text-accent" style={{ fontFamily: "Poppins" }}>
+                  Personal Details
+                </Text>
+              </View>
 
-          <TouchableOpacity className="flex-row items-center justify-between py-2">
-            <View className="flex-row items-center gap-4">
-              <Feather name="settings" size={24} color="#6200EE" />
+              <View className="p-2">
+                <FontAwesome6 name="angle-right" size={20} color="#767676" />
+              </View>
+            </TouchableOpacity>
 
-              <Text className="text-accent" style={{ fontFamily: "Poppins" }}>
-                Settings
-              </Text>
-            </View>
+            <TouchableOpacity className="flex-row items-center justify-between py-2">
+              <View className="flex-row items-center gap-4">
+                <Feather name="settings" size={24} color="#767676" />
 
-            <View className="p-2">
-              <FontAwesome6 name="angle-right" size={20} color="#767676" />
-            </View>
-          </TouchableOpacity>
-        </View>
+                <Text className="text-accent" style={{ fontFamily: "Poppins" }}>
+                  Settings
+                </Text>
+              </View>
 
-        {/* help and support */}
-        <View className="mt-8">
-          <Text
-            className="text-2xl font-medium text-primary mb-4"
-            style={{ fontFamily: "Poppins" }}
-          >
-            Help & Support
-          </Text>
+              <View className="p-2">
+                <FontAwesome6 name="angle-right" size={20} color="#767676" />
+              </View>
+            </TouchableOpacity>
+          </View>
 
-          <TouchableOpacity className="flex-row items-center justify-between py-2 mb-4">
-            <View className="flex-row items-center gap-4">
-              <SimpleLineIcons name="question" size={24} color="#6200EE" />
+          {/* help and support */}
+          <View className="mt-8">
+            <Text
+              className="text-2xl font-medium text-primary mb-4"
+              style={{ fontFamily: "Poppins" }}
+            >
+              Help & Support
+            </Text>
 
-              <Text className="text-accent" style={{ fontFamily: "Poppins" }}>
-                FAQs
-              </Text>
-            </View>
+            <TouchableOpacity className="flex-row items-center justify-between py-2 mb-4">
+              <View className="flex-row items-center gap-4">
+                <SimpleLineIcons name="question" size={24} color="#767676" />
 
-            <View className="p-2">
-              <FontAwesome6 name="angle-right" size={20} color="#767676" />
-            </View>
-          </TouchableOpacity>
+                <Text className="text-accent" style={{ fontFamily: "Poppins" }}>
+                  FAQs
+                </Text>
+              </View>
 
-          <TouchableOpacity className="flex-row items-center justify-between py-2 mb-4">
-            <View className="flex-row items-center gap-4">
-              <MaterialCommunityIcons
-                name="file-lock-outline"
-                size={24}
-                color="#6200EE"
-              />
+              <View className="p-2">
+                <FontAwesome6 name="angle-right" size={20} color="#767676" />
+              </View>
+            </TouchableOpacity>
 
-              <Text className="text-accent" style={{ fontFamily: "Poppins" }}>
-                Privacy Policy
-              </Text>
-            </View>
+            <TouchableOpacity className="flex-row items-center justify-between py-2 mb-4">
+              <View className="flex-row items-center gap-4">
+                <MaterialCommunityIcons
+                  name="file-lock-outline"
+                  size={24}
+                  color="#767676"
+                />
 
-            <View className="p-2">
-              <FontAwesome6 name="angle-right" size={20} color="#767676" />
-            </View>
-          </TouchableOpacity>
+                <Text className="text-accent" style={{ fontFamily: "Poppins" }}>
+                  Privacy Policy
+                </Text>
+              </View>
 
-          <TouchableOpacity className="flex-row items-center justify-between py-2">
-            <View className="flex-row items-center gap-4">
-              <Feather name="file-text" size={24} color="#6200EE" />
+              <View className="p-2">
+                <FontAwesome6 name="angle-right" size={20} color="#767676" />
+              </View>
+            </TouchableOpacity>
 
-              <Text className="text-accent" style={{ fontFamily: "Poppins" }}>
-                Terms & Conditions
-              </Text>
-            </View>
+            <TouchableOpacity className="flex-row items-center justify-between py-2">
+              <View className="flex-row items-center gap-4">
+                <Feather name="file-text" size={24} color="#767676" />
 
-            <View className="p-2">
-              <FontAwesome6 name="angle-right" size={20} color="#767676" />
-            </View>
-          </TouchableOpacity>
+                <Text className="text-accent" style={{ fontFamily: "Poppins" }}>
+                  Terms & Conditions
+                </Text>
+              </View>
+
+              <View className="p-2">
+                <FontAwesome6 name="angle-right" size={20} color="#767676" />
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </SafeScreen>
