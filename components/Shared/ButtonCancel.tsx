effaa548de72f4362c2a02981300cd759d83e16a
xyryc <amdanikzz@gmail.com>
2025-08-20 03:21:31 +0600

    button UI updated

--- components/Shared/ButtonCancel.tsx
+++ components/Shared/ButtonCancel.tsx
@@ -6,12 +6,12 @@ const ButtonCancel = ({
   icon,
   text,
   onPress,
-  className = "rounded-2xl",
+  className = "rounded-[4px]",
 }: ButtonSmallProps) => {
   return (
     <TouchableOpacity
       onPress={onPress}
-      className={` overflow-hidden w-[48%] border border-[#CE0D0D] ${className}`}
+      className={`overflow-hidden w-[48%] border border-[#CE0D0D] ${className}`}
     >
       <View className="flex-row items-center justify-center gap-2.5 py-2">
         {icon && icon}
