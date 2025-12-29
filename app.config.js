1c9a342931ebe3c71ca80bedf711415970cc2765
xyryc <amdanikzz@gmail.com>
2025-12-29 22:57:54 +0600

    update app.config.js

--- app.config.js
+++ app.config.js
@@ -20,6 +20,7 @@ export default {
       }
     },
     "android": {
+      "minSdkVersion": 24,
       "adaptiveIcon": {
         "foregroundImage": "./assets/images/adaptive-icon.png",
         "backgroundColor": "#ffffff"
